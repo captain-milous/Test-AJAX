3939b4f573d35b3f20d012eaf549db1567641292
captain-milous <milda.tesar3@gmail.com>
2023-03-30 08:26:08 +0200

    btn

--- myscript.js
+++ myscript.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
 	$("#loading").hide();
 	$.getJSON("https://v2.jokeapi.dev/joke/Any?type=single", function(data) {
-		
+
 	    $("#joke").text(data.joke);
 	});
 
 
+	$('#btn-addToLiked').on('click', function(e) {
+		// kód
+	});
+
+	$('#btn-addToDisliked').on('click', function(e) {
+		// kód
+	});
+
+	$('#btn-newAPI').on('click', function(e) {
+		// kód
+	});
+
+	$('#"btn-liked').on('click', function(e) {
+		// kód
+	});
+
+	$('#"btn-disliked').on('click', function(e) {
+		// kód
+	});
+
 });
-//--------------------------------------
+
