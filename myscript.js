7040f2cdae8d0ecc66e558ac6bb816604b4752b7
captain-milous <milda.tesar3@gmail.com>
2023-03-30 08:54:41 +0200

    functions

--- myscript.js
+++ myscript.js
@@ -1,30 +1,58 @@
 $(document).ready(function() {
-	$("#loading").hide();
-	$.getJSON("https://v2.jokeapi.dev/joke/Any?type=single", function(data) {
+	var liked = [];
+	var disliked = [];
+	var current= "";
 
-	    $("#joke").text(data.joke);
+	Hide();
+	NewAPI();
+
+	$('#btn-addToLiked').click(function(e) {
+		Hide();
+		liked.push(current);
+		NewAPI();
 	});
 
-
-	$('#btn-addToLiked').on('click', function(e) {
-		// kód
+	$('#btn-addToDisliked').click(function(e) {
+		Hide();
+		disliked.push(current);
+		NewAPI();
 	});
 
-	$('#btn-addToDisliked').on('click', function(e) {
-		// kód
+	$('#btn-newAPI').click(function(e) {
+		Hide();
+		NewAPI();
+		console.log(current);
 	});
 
-	$('#btn-newAPI').on('click', function(e) {
-		// kód
+	$('#btn-liked').click(function(e) {
+		Hide();
+		
+		$("#myLiked").show();
+		console.log(liked);
 	});
 
-	$('#"btn-liked').on('click', function(e) {
-		// kód
+	$('#btn-disliked').click(function(e) {
+		Hide();
+
+		$("#myDisliked").show();
+		console.log(disliked);
 	});
 
-	$('#"btn-disliked').on('click', function(e) {
-		// kód
-	});
+	function NewAPI() {
+		$("#loading").show();
+		$("#joke").text("");
+		$.getJSON("https://v2.jokeapi.dev/joke/Any?type=single", function(data) {
+			current = data.joke;
+			$("#loading").hide();
+			$("#joke").text(current);
+		});
+	}
 
+	function Hide() {
+		$("#myLiked").hide();
+		$("#myDisliked").hide();
+	}
 });
+
+
 
